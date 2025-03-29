457fbbeea2587b6d049971f4b05d0cae86c3cd46
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-03-30 01:15:15 +0600

    local storage

--- src/Bottles/Bottles.jsx
+++ src/Bottles/Bottles.jsx
@@ -1,16 +1,34 @@
-import React, { use, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
+import { addStoredCart, getStoredCart } from "../utilities/localstorage";
 
 const Bottles = ({ bottleData }) => {
   const data = use(bottleData);
 
   const [cart, setCart] = useState([]);
 
+  // useEffect
+  useEffect(() => {
+    const storedCardIds = getStoredCart();
+    const storedCart = [];
+    // console.log(storedCardIds,data);
+    for (const id of storedCardIds) {
+      const cartBottle = data.find((bottle) => bottle.id === id);
+      if (cartBottle) {
+        storedCart.push(cartBottle);
+      }
+    }
+    setCart(storedCart);
+  }, [data]);
+
   const handleAddToCart = (bottle) => {
     const newCart = [...cart, bottle];
     setCart(newCart);
     console.log(bottle);
+
+    //   save the bottle id to the store date;
+    addStoredCart(bottle.id);
   };
 
   return (
--- src/utilities/localstorage.js
+++ src/utilities/localstorage.js
@@ -1,3 +1,23 @@
 const getCartFromLocalStorage = () => {
   const cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(cart);
+  }
+  return [];
+};
+
+const saveCartToLocalStorage = (cart) => {
+  const cartstring = JSON.stringify(cart);
+  localStorage.setItem("cart", cartstring);
+};
+
+const addItemToLocalStorage = (item) => {
+  const cart = getCartFromLocalStorage();
+  const newCart = [...cart, item];
+  saveCartToLocalStorage(newCart);
+};
+
+export {
+  getCartFromLocalStorage as getStoredCart,
+  addItemToLocalStorage as addStoredCart,
 };
